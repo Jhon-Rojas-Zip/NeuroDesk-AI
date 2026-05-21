88f01b282896f14aa2310935795b0274e16ebf75
Jhon-Rojas-Zip <rojashenaojhonedison@gmail.com>
2026-05-20 23:46:27 -0500

    Importando primeras bases

---
+++ app.js
@@ -0,0 +1,73 @@
+const taskInput = document.getElementById('taskInput');
+const addTaskBtn = document.getElementById('addTaskBtn');
+const taskList = document.getElementById('taskList');
+const notesArea = document.getElementById('notesArea');
+
+let tasks = JSON.parse(localStorage.getItem('neuroTasks')) || [];
+
+function saveTasks() {
+    localStorage.setItem('neuroTasks', JSON.stringify(tasks));
+}
+
+function renderTasks() {
+
+    taskList.innerHTML = '';
+
+    tasks.forEach((task, index) => {
+
+        const li = document.createElement('li');
+
+        li.classList.add('task-item');
+
+        li.innerHTML = `
+            <span>${task}</span>
+            <button onclick="deleteTask(${index})">
+                <i class="fa-solid fa-trash"></i>
+            </button>
+        `;
+
+        taskList.appendChild(li);
+    });
+}
+
+function addTask() {
+
+    const value = taskInput.value.trim();
+
+    if(value === '') return;
+
+    tasks.push(value);
+
+    saveTasks();
+    renderTasks();
+
+    taskInput.value = '';
+}
+
+function deleteTask(index) {
+
+    tasks.splice(index, 1);
+
+    saveTasks();
+    renderTasks();
+}
+
+addTaskBtn.addEventListener('click', addTask);
+
+window.deleteTask = deleteTask;
+
+renderTasks();
+
+// NOTES
+
+notesArea.value = localStorage.getItem('neuroNotes') || '';
+
+notesArea.addEventListener('input', () => {
+    localStorage.setItem('neuroNotes', notesArea.value);
+});
+
+// CHART
+
+const ctx = document.getElementById('productivityChart');
+
+});
